eb27d0f9d5641e13a0bb38e6a6bd6eaf29e0b4a2
chenmo <chenmo@speakin.ai>
2021-12-03 15:03:37 +0800

    <complete>

--- dove.go
+++ dove.go
@@ -1,6 +1,7 @@
 package dove
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/base64"
 	"errors"
@@ -15,35 +16,8 @@ const (
 	_FILE   = 0x01
 	_HTTP   = 0x02
 	_HTTPS  = 0x04
-	_FTP    = 0x08
-	_BASE64 = 0x10
+	_BASE64 = 0x08
 )
-
-type Dover interface {
-	Throw(error) Dover
-	Drop() Dover
-	Return() Dover
-	Next(int) Dover
-	Util(string) Dover
-	Buffer(int) Dover
-	Get(int) (interface{}, error)
-	Ok() bool
-}
-
-type ecmd struct {
-	e   error
-	cmd string
-}
-
-type dove struct {
-	ecmd   ecmd
-	next   int
-	params []interface{}
-	buffer []byte
-	ok     bool
-	memory bool
-	reader io.ReadCloser
-}
 
 type tinfo struct {
 	no       int
@@ -52,121 +26,35 @@ type tinfo struct {
 	bases    []byte
 }
 
-func (m *dove) Throw(err error) Dover {
-	m.ecmd.e = err
-	return m
-}
+func Fly(url string, bsize int, f func(rd *bufio.Reader, objs ...interface{}) error, objs ...interface{}) (err error) {
 
-func (m *dove) Drop() Dover {
-	m.next += len(m.buffer)
-	return m
-}
-
-func (m *dove) Return() Dover {
-	m.ecmd.cmd = "return"
-	return m
-}
-
-func (m *dove) Next(n int) Dover {
-	m.next = n
-	return m
-}
-
-func (m *dove) Util(end string) Dover {
-	return m
-}
-
-func (m *dove) Buffer(n int) Dover {
-
-	tmp := m.buffer[:0]
-	copy(tmp, m.buffer)
-	m.buffer = make([]byte, n)
-	copy(m.buffer, tmp)
-
-	if len(tmp) > 0 {
-		_, _ = io.CopyBuffer(bytes.NewBuffer(m.buffer), m.reader, tmp)
-	}
-
-	return m
-}
-
-func (m *dove) Ok() bool {
-	return m.ok
-}
-
-func (m *dove) Get(n int) (interface{}, error) {
-
-	if len(m.params) == 0 {
-		return nil, errors.New("has no object")
-	}
-
-	if n < 0 || n >= len(m.params) {
-		return nil, errors.New("invalid index")
-	}
-
-	return m.params[n], nil
-}
-
-func (m *dove) lazyRead(b []byte) (n int, err error) {
-	return m.reader.Read(b)
-}
-
-func Fly(uri string, f func(i Dover, n int, b []byte) (o Dover), objs ...interface{}) (err error) {
-
-	i, n, b := &dove{params: objs}, 0, []byte{}
-
-	defer catch(i, &err, n, b)
-
-	var o = f(i, n, b)
-	o.(*dove).ok = true
-	b = make([]byte, len(o.(*dove).buffer))
-
-	reader, err := o.(*dove).getReader(uri)
+	reader, err := getReader(url)
 	if err != nil {
 		return err
 	}
 	defer reader.Close()
 
-	i.reader = reader
-
-	for {
-
-		i = o.(*dove)
-		if i.ecmd.e != nil {
-			err = i.ecmd.e
-			return
-		}
-
-		if i.ecmd.cmd == "return" {
-			return
-		}
-
-		n, err = i.lazyRead(b)
-
-		if err == io.EOF {
-			return nil
-		}
-
-		if err != nil {
-			return err
-		}
-
-		o = f(i, n, b)
-
+	if bsize <= 0 {
+		bsize = 4096
 	}
+
+	return f(bufio.NewReaderSize(reader, bsize), objs...)
+
 }
 
-func (m *dove) getReader(uri string) (io.ReadCloser, error) {
+func getReader(url string) (io.ReadCloser, error) {
 
-	switch info := guess(uri); info.no {
+	info, err := guess(url)
+	if err != nil {
+		return nil, err
+	}
+
+	switch info.no {
 	case _HTTP:
 		return info.response.Body, nil
 	case _HTTPS:
 		return info.response.Body, nil
-	case _FTP:
-		return nil, errors.New("unsupport type")
 	case _BASE64:
-		m.memory = true
 		return io.NopCloser(bytes.NewReader(info.bases)), nil
 	case _FILE:
 		return info.file, nil
@@ -175,50 +63,34 @@ func (m *dove) getReader(uri string) (io.ReadCloser, error) {
 	return nil, errors.New("unsupport type")
 }
 
-func guess(uri string) tinfo {
+func guess(url string) (tinfo, error) {
 
-	if strings.HasPrefix(uri, "https://") {
-		resp, err := http.Get(uri)
+	if strings.HasPrefix(url, "https://") {
+		resp, err := http.Get(url)
 		if err != nil {
-			return tinfo{}
+			return tinfo{}, err
 		}
-		return tinfo{no: _HTTPS, response: resp}
+		return tinfo{no: _HTTPS, response: resp}, nil
 	}
 
-	if strings.HasPrefix(uri, "http://") {
-		resp, err := http.Get(uri)
+	if strings.HasPrefix(url, "http://") {
+		resp, err := http.Get(url)
 		if err != nil {
-			return tinfo{}
+			return tinfo{}, err
 		}
-		return tinfo{no: _HTTP, response: resp}
+		return tinfo{no: _HTTP, response: resp}, nil
 	}
 
-	if strings.HasPrefix(uri, "ftp://") {
-		return tinfo{no: _FTP}
+	if f, err := os.Open(url); !errors.Is(err, fs.ErrNotExist) {
+		return tinfo{no: _FILE, file: f}, nil
+	} else if err != nil {
+		return tinfo{}, err
 	}
 
-	if f, err := os.Open(uri); !errors.Is(err, fs.ErrNotExist) {
-		return tinfo{no: _FILE, file: f}
+	if s, err := base64.StdEncoding.DecodeString(url); err == nil {
+		return tinfo{no: _BASE64, bases: s}, nil
+	} else {
+		return tinfo{}, err
 	}
 
-	if s, err := base64.StdEncoding.DecodeString(uri); err == nil {
-		return tinfo{no: _BASE64, bases: s}
-	}
-
-	return tinfo{}
-}
-
-func catch(d *dove, exp *error, n int, b []byte) {
-
-	if obj := recover(); obj != nil {
-
-		if e, ok := obj.(error); ok && !d.ok &&
-			strings.Contains(e.Error(), "runtime error: index out of range") {
-
-			*exp = errors.New("call Dover's Ok function first ")
-			return
-		}
-
-		panic(obj)
-	}
 }
--- dove_test.go
+++ dove_test.go
@@ -1,31 +1,21 @@
 package dove
 
 import (
+	"bufio"
+	"os"
 	"testing"
 )
 
 func TestDove(t *testing.T) {
 
 	err := Fly(
-		"E:\\logs\\nlp-service.log",
-		// "https://pkg.go.dev/fmt",
-		// "http://192.168.0.206/arm/vpr_onnx_4.1.4_Linux_aarch64_20210715.tar.gz",
+		"https://pkg.go.dev/fmt",
+		1024,
 
-		func(i Dover, n int, b []byte) (o Dover) {
-			// v1, _ := i.Get(0)
-			// t.Log(v1.(int))
-			// v2, _ := i.Get(1)
-			// t.Log(v2.(int))
-			// v3, _ := i.Get(2)
-			// t.Log(v3.(int))
-			if i.Ok() {
-				t.Log("\n", string(b))
-				return i.Next(1).Buffer(32)
-			}
-			return i.Next(1).Buffer(128)
+		func(rd *bufio.Reader, objs ...interface{}) error {
+			_, err := rd.WriteTo(os.Stdout)
+			return err
 		},
-
-		// 1, 2, 3,
 	)
 
 	if err != nil {
